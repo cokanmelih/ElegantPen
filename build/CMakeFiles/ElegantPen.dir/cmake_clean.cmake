844eb6e60d7d2712a96674fdadc0bbe4886cdde0
cokanmelih <melihcokan@icloud.com>
2023-02-12 21:55:21 +0300

    initial commit

---
+++ build/CMakeFiles/ElegantPen.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ElegantPen.dir/main.cpp.o"
+  "CMakeFiles/ElegantPen.dir/main.cpp.o.d"
+  "ElegantPen"
+  "ElegantPen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ElegantPen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
